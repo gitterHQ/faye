529302dda80719993d777ff86d927f482eb3515d
Andrew Newdigate <andrew@troupe.co>
2014-06-03 11:13:40 +0100

    Only copy on tests passing

--- monitor.sh
+++ monitor.sh
@@ -6,5 +6,7 @@ set -x
 fswatch -0 . --exclude=.git --exclude=.wake.json --exclude=build/ --exclude=opensource/faye/lib/| while read -d "" event; do
   echo change $event;
   wake;
-  cp build/browser/faye-browser.js ../../gitter/webapp/public/repo/faye/faye.js;
+  if node spec/node.js; then
+    cp build/browser/faye-browser.js ../../gitter/webapp/public/repo/faye/faye.js;
+  fi
 done
